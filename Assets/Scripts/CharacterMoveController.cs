09e24fb7d3b0027545f23021d465d6b5a8074de1
ilhamdaffa21 <imdaffa2001@gmail.com>
2021-09-15 12:02:23 +0700

    Initial commit

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    private bool isJumping;
+    private bool isOnGround;
+    private bool isDead=false;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+
+    [Header("Obstacle")]
+    public GameObject obstacle;
+
+    [Header("GameOver")]
+    public GameObject gameOverScreen;
+    public float fallPositionY;
+
+    [Header("Camera")]
+    public CameraMoveController gameCamera;
+
+    [Header("BGM")]
+    public AudioSource bgm;
+
+    private Animator anim;
+    private Rigidbody2D rig;
+    private CharacterSoundController sound;
+
+    private float lastPositionX;
+
+    
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+        bgm = GetComponent<AudioSource>();
+        lastPositionX = transform.position.x;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+                sound.PlayJump();
+            }
+        }
+        
+        anim.SetBool("isOnGround", isOnGround);
+
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if(scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+
+        if(transform.position.y< fallPositionY)
+        {
+            GameOver();
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        if (anotherCollider.name == "obstcl")
+        {
+            GameOver();
+            anim.SetBool("isDead", true);
+            //AnimationStop();
+        }
+    }
+
+    private void AnimationStop()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        //raycast
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        Vector2 velocityVector = rig.velocity;
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = velocityVector;
+    }
+
+    private void GameOver()
+    {
+        score.FinishScoring();
+        gameCamera.enabled = false;
+        gameOverScreen.SetActive(true);
+        this.enabled = false;
+        bgm.Stop();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    private int currentScore = 0 ;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+    }
+
+    public void FinishScoring()
+    {
+        if (currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+
+}
